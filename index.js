bdf6caea80764550848a9fabce4e82a98c57533e
asymptoter <asymptotion@gmail.com>
2020-05-15 02:48:43 +0800

    Test http get on github pages

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function login() {
-    
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "http://asymptoter-practice.nctu.me:8080/ping", false ); // false for synchronous request
+    xmlHttp.send( null );
+    console.log(xmlHttp.responseText);
 }
